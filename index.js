0f472242be067f9c5d51255952303c27dc77962a
Jose Dominguez <jdomindev@gmail.com>
2022-01-04 22:04:32 -0500

    added more markdown to generateMarkdown.js, add objects to questions array in index.js

--- index.js
+++ index.js
@@ -1,14 +1,80 @@
 // TODO: Include packages needed for this application
-// const inquirer = require inquirer
+// const inquirer = require "inquirer"
+// const fs = require("fs");
+// const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
 // array of objects with types
-const questions = [{type: ex, object: ex, name: ex,}, {}];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is your username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is a description for your project?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What is needed for the installation of your project?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What is needed for the usage of your project?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Are there any credits for your project, if so who or what?",
+    name: "credits",
+  },
+  {
+    type: "input",
+    message: "Choose a license to use for your project?",
+    name: "",
+  },
+  {
+    type: "input",
+    message: "What are the features of your project?",
+    name: "features",
+  },
+  {
+    type: "input",
+    message: "What can others do to contribute to your project?",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message: "Write any tests for your project.",
+    name: "tests",
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+//     fs.writeFile(path.join(__dirname, "./readme-generator", "README.md"), generateMarkdown, err => {
+//     if (err) throw err;
+//     console.log("README written...");
+// })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
+
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,37 @@
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    // return `![License](https://img.shields.io/badge/license-${license}-lightgrey)`
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    // return `![License](https://img.shields.io/badge/license-${license}-lightgrey)`
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,15 +40,38 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  # ${data.description}
-  # ${data.tableOfContents}
-  # ${data.installation}
-  # ${data.usage}
-  # ${data.credits}
-  # ${data.license}
-  # ${data.badges}
-  # ${data.features}
-  # ${data.contribute}
+  # Description
+  ${data.description}
+  # Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Badges](#badges)
+  - [Features](#features)
+  - [Contribute](#contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  # Installation
+  ${data.installation}
+  # Usage
+  ${data.usage}
+  - Github Repo: https://github.com/${data.username}/${data.title}
+  # Credits
+  ${data.credits}
+  # License
+  // renderLicenseSection()
+  # Badges 
+  // renderLicenseBadge()
+  # Features
+  ${data.features}
+  # How to Contribute
+  ${data.contribute}
+  # Tests
+  ${data.tests}
+  # Questions
+  - Github: https://github.com/${data.username}
+  - Any questions can be answered by emailing me at ${data.email}
 `;
 }
 
